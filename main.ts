f1a791c8c5894e12e0f9e4eb467044cf74ecbf72
ed123-abc <78638924+ed123-abc@users.noreply.github.com>
2021-11-01 12:10:16 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showIcon(IconNames.EigthNote)
+basic.forever(function () {
+    soundExpression.giggle.playUntilDone()
+    basic.pause(1000)
+    soundExpression.happy.playUntilDone()
+    basic.pause(1000)
+    soundExpression.hello.playUntilDone()
+    basic.pause(1000)
+    soundExpression.yawn.playUntilDone()
+    basic.pause(1000)
+})
